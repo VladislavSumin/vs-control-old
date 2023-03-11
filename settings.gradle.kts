e38eeceff3295467bc6d9011e12517ff49e6b30d
Vladislav Sumin <vladislavsumin@mail.ru>
2023-03-11 22:54:25 +0300

    enable mavenLocal repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
     includeBuild("build-logic")
 
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         google()
         mavenCentral()
@@ -13,6 +14,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
         google()
         mavenCentral()
     }
