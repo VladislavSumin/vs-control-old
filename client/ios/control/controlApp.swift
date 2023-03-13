37a264751e7b5d5ea518065e45ceabc9e5ec032f
Vladislav Sumin <vladislavsumin@mail.ru>
2023-03-13 20:23:47 +0300

    init ios app

---
+++ client/ios/control/controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  controlApp.swift
+//  control
+//
+//  Created by Vladislav Sumin on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
