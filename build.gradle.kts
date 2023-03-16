3a505911905415f2908ec42d2fc24a81581bc9eb
Vladislav Sumin <vladislavsumin@mail.ru>
2023-03-16 23:33:53 +0300

    add ios binaries build on CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@ tasks.register("ci") {
     dependsOn(":client:android:assembleDebug")
     dependsOn(":client:android:assembleRelease")
     dependsOn(":client:jvm:buildFatJar")
+
+    dependsOn(":client:common:iosArm64MainBinaries")
 }
