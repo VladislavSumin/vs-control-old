862b6eff35b9645fdfd002bfd2616e205a5c2863
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-16 21:32:15 +0300

    fix issue with composite builds see https://youtrack.jetbrains.com/issue/KT-52172/Multiplatform-Support-composite-builds

--- build-logic/src/main/kotlin/ru/vs/convention/preset/server-feature.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/preset/server-feature.gradle.kts
@@ -1,7 +1,23 @@
 package ru.vs.convention.preset
 
+import org.gradle.accessors.dm.LibrariesForCoreLibs
+
 plugins {
     id("ru.vs.convention.kmp.jvm")
     id("ru.vs.convention.kmp.linux")
     id("ru.vs.convention.kmp.macos")
+}
+
+val coreLibs = rootProject.the<LibrariesForCoreLibs>()
+
+kotlin {
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                 implementation(coreLibs.vs.core.logging)
+                 implementation(coreLibs.vs.core.di)
+                 implementation(coreLibs.vs.core.ktor.server)
+            }
+        }
+    }
 }
