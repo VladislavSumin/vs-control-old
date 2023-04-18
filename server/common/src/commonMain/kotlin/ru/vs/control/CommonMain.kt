b3d957be584cc989e282ea76ce490531784b7222
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-18 21:56:48 +0300

    setup simple logging

--- server/common/src/commonMain/kotlin/ru/vs/control/CommonMain.kt
+++ server/common/src/commonMain/kotlin/ru/vs/control/CommonMain.kt
@@ -1,6 +1,10 @@
 package ru.vs.control
 
+import io.github.oshai.KotlinLogging
+
 fun commonMain() {
+    val logger = KotlinLogging.logger("main")
+    logger.info { "Server starting..." }
     val scope = ServerScope()
     println("Hello server")
     scope.blockingAwait()
