62a8ba07bec0b73c10b320ed4ec29c684053a27e
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-20 20:52:12 +0300

    add js target for compose

--- build-logic/src/main/kotlin/ru/vs/convention/preset/client-feature.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/preset/client-feature.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("ru.vs.convention.kmp.android-library")
     id("ru.vs.convention.kmp.jvm")
     id("ru.vs.convention.kmp.ios")
-    // id("ru.vs.convention.kmp.js") compose (for desktop) don't support js now
+    id("ru.vs.convention.kmp.js")
     // id("ru.vs.convention.kmp.windows") compose (for desktop) don't support windows now
     // id("ru.vs.convention.kmp.linux") compose (for desktop) don't support linux now
     id("ru.vs.convention.kmp.macos")
