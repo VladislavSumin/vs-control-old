0a966f77486abdc93f4b83ed17972afca1bd086b
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-24 21:01:58 +0300

    setup compose jvm

--- client/jvm/build.gradle.kts
+++ client/jvm/build.gradle.kts
@@ -2,6 +2,7 @@ import ru.vs.build_logic.utils.fatJar
 
 plugins {
     id("ru.vs.convention.kmp.jvm")
+    id("org.jetbrains.compose")
 }
 
 val clientMainClass: String = "ru.vs.control.MainKt"
@@ -15,6 +16,11 @@ kotlin {
         named("commonMain") {
             dependencies {
                 implementation(projects.client.common)
+
+                implementation(coreLibs.vs.core.compose)
+                implementation(coreLibs.vs.core.coroutines)
+                implementation(coreLibs.vs.core.di)
+                implementation(coreLibs.vs.core.logging)
             }
         }
     }
--- client/jvm/src/jvmMain/kotlin/ru/vs/control/Main.kt
+++ client/jvm/src/jvmMain/kotlin/ru/vs/control/Main.kt
@@ -1,5 +1,14 @@
 package ru.vs.control
 
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
 fun main(args: Array<String>) {
     println(HelloProvider.getHello())
+
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            HelloView()
+        }
+    }
 }
