d1083390d312a5d377591047751a978905427f62
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-25 21:57:51 +0300

    remove unused import

--- client/common/src/commonMain/kotlin/ru/vs/control/ui/root/RootContent.kt
+++ client/common/src/commonMain/kotlin/ru/vs/control/ui/root/RootContent.kt
@@ -3,7 +3,6 @@ package ru.vs.control.ui.root
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
-import ru.vs.control.HelloView
 import ru.vs.control.servers.ui.servers.ServersContent
 
 @Composable
