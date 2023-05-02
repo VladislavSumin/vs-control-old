52c08e9f66d6bda5b50640425e35efbcce972786
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-02 21:19:04 +0300

    add android support for LocalConfiguration

--- client/android/src/main/kotlin/ru/vs/control/App.kt
+++ client/android/src/main/kotlin/ru/vs/control/App.kt
@@ -1,12 +1,15 @@
 package ru.vs.control
 
 import android.app.Application
+import android.content.Context
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import org.kodein.di.bindSingleton
 import ru.vs.core.di.Modules
 
 class App : Application(), DIAware {
     override val di: DI = DI.lazy {
+        bindSingleton<Context> { this@App }
         importOnce(Modules.clientCommon())
     }
 }
