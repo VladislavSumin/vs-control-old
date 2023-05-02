d674339fbd92b80e0577d713b0731521c39896f5
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-02 19:43:31 +0300

    setup main screen drawer basic design

--- feature/main-screen/client-impl/src/commonMain/kotlin/ru/vs/control/main_screen/ui/main_screen/MainScreenContent.kt
+++ feature/main-screen/client-impl/src/commonMain/kotlin/ru/vs/control/main_screen/ui/main_screen/MainScreenContent.kt
@@ -1,17 +1,11 @@
 package ru.vs.control.main_screen.ui.main_screen
 
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,16 +28,5 @@ private fun MainScreenNavigation(component: MainScreenComponent) {
         stack = component.stack,
     ) {
         it.instance.Render()
-    }
-}
-
-@Composable
-private fun MainScreenDrawerContent() {
-    Surface(
-        Modifier
-            .width(200.dp)
-            .fillMaxHeight()
-    ) {
-        Text("Drawer content")
     }
 }
---
+++ feature/main-screen/client-impl/src/commonMain/kotlin/ru/vs/control/main_screen/ui/main_screen/MainScreenDrawerContent.kt
@@ -0,0 +1,59 @@
+package ru.vs.control.main_screen.ui.main_screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MainScreenDrawerContent() {
+    ModalDrawerSheet {
+        Column(Modifier.padding(horizontal = 16.dp, vertical = 12.dp)) {
+            Header(Modifier.fillMaxWidth())
+            Divider()
+
+            NavigationDrawerItem(
+                label = { Text("Drawer content") },
+                selected = true,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+private fun Header(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Icon(
+            Icons.Default.AccountBox,
+            contentDescription = null,
+            Modifier
+                .size(56.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            "Control",
+            Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        Text(
+            "control.vs.ru:8080",
+            Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
