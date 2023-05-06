89afb961959ad7ddf9ef6f3d71fd6ca6009cf8c5
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-06 17:44:28 +0300

    add more tests to Id

--- core/id/src/commonMain/kotlin/ru/vs/control/id/Id.kt
+++ core/id/src/commonMain/kotlin/ru/vs/control/id/Id.kt
@@ -1,12 +1,6 @@
 package ru.vs.control.id
 
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
 @Serializable(IdSerializer::class)
 interface Id {
@@ -17,17 +11,19 @@ interface Id {
     }
 }
 
-internal class IdImpl(override val rawId: String) : Id
+internal class IdImpl(override val rawId: String) : Id {
 
-internal object IdSerializer : KSerializer<Id> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("id", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): Id {
-        val rawId = decoder.decodeString()
-        return Id(rawId)
+    init {
+        check(ID_VERIFICATION_REGEXP.matches(rawId)) { "Incorrect id format" }
     }
 
-    override fun serialize(encoder: Encoder, value: Id) {
-        encoder.encodeString(value.rawId)
+    override fun toString(): String {
+        return "Id($rawId)"
+    }
+
+    companion object {
+        private const val ID_PART_VERIFICATION_REGEXP = "([a-z]+(_[a-z]+)*)"
+        private val ID_VERIFICATION_REGEXP =
+            "^$ID_PART_VERIFICATION_REGEXP(/$ID_PART_VERIFICATION_REGEXP.)*\$".toRegex()
     }
 }
--- core/id/src/commonTest/kotlin/ru/vs/control/id/IdTest.kt
+++ core/id/src/commonTest/kotlin/ru/vs/control/id/IdTest.kt
@@ -2,20 +2,68 @@ package ru.vs.control.id
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class IdTest {
     @Test
-    fun createWithCorrectId() {
-        Id(TEST_RAW_ID)
+    fun createWithCorrectSimpleId() {
+        Id(TEST_RAW_ID_SIMPLE)
+    }
+
+    @Test
+    fun createWithCorrectPartsId() {
+        Id(TEST_RAW_ID_WITH_PARTS)
+    }
+
+    @Test
+    fun createWithCorrectComplexId() {
+        Id(TEST_RAW_ID_COMPLEX)
+    }
+
+    @Test
+    fun createWithIncorrectIdEmptyString() {
+        assertFailsWith<IllegalStateException> {
+            Id("")
+        }
+    }
+
+    @Test
+    fun createWithIncorrectIdStartAtSlash() {
+        assertFailsWith<IllegalStateException> {
+            Id("/a/b/c")
+        }
+    }
+
+    @Test
+    fun createWithIncorrectIdEndAtSlash() {
+        assertFailsWith<IllegalStateException> {
+            Id("a/b/c/")
+        }
+    }
+
+    @Test
+    fun createWithIncorrectIdContainsSpace() {
+        assertFailsWith<IllegalStateException> {
+            Id("a a/b")
+        }
+    }
+
+    @Test
+    fun createWithIncorrectIdContainsUpperRegister() {
+        assertFailsWith<IllegalStateException> {
+            Id("aA")
+        }
     }
 
     @Test
     fun rawIdEqualsCreationId() {
-        val id = Id(TEST_RAW_ID)
-        assertEquals(TEST_RAW_ID, id.rawId)
+        val id = Id(TEST_RAW_ID_SIMPLE)
+        assertEquals(TEST_RAW_ID_SIMPLE, id.rawId)
     }
 
     companion object {
-        private const val TEST_RAW_ID = "test/id/with/same/path"
+        private const val TEST_RAW_ID_SIMPLE = "test_id"
+        private const val TEST_RAW_ID_WITH_PARTS = "test/id/with/same/path"
+        private const val TEST_RAW_ID_COMPLEX = "test/id/with_same_path"
     }
 }
