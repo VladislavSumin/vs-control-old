137652c42547195e4a3b73aa10ca4ef97b853388
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-13 15:32:03 +0300

    update feature-shared convention

--- build-logic/src/main/kotlin/ru/vs/convention/preset/feature-shared-api.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/preset/feature-shared-api.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.accessors.dm.LibrariesForCoreLibs
 
 plugins {
     id("ru.vs.convention.kmp.all")
+    id("ru.vs.convention.serialization.json")
 }
 
 val coreLibs = rootProject.the<LibrariesForCoreLibs>()
--- build-logic/src/main/kotlin/ru/vs/convention/preset/feature-shared-impl.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/preset/feature-shared-impl.gradle.kts
@@ -1,14 +1,18 @@
 package ru.vs.convention.preset
 
+import org.gradle.accessors.dm.LibrariesForCoreLibs
+
 plugins {
     id("ru.vs.convention.preset.feature-shared-api")
-    id("ru.vs.convention.serialization.json")
 }
+
+val coreLibs = rootProject.the<LibrariesForCoreLibs>()
 
 kotlin {
     sourceSets {
         named("commonMain") {
             dependencies {
+                implementation(coreLibs.vs.core.di)
                 implementation(project(":rsub:core"))
             }
         }
