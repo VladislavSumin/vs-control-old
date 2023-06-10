2340e54b889dc2e0f071a373f9a8243eee333c48
Vladislav Sumin <vladislavsumin@mail.ru>
2023-06-10 16:51:29 +0300

    fix entities screen padding

--- feature/entities/client-impl/src/commonMain/kotlin/ru/vs/control/entities/ui/entities/EntitiesContent.kt
+++ feature/entities/client-impl/src/commonMain/kotlin/ru/vs/control/entities/ui/entities/EntitiesContent.kt
@@ -1,23 +1,31 @@
 package ru.vs.control.entities.ui.entities
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun EntitiesContent(component: EntitiesComponent) {
-    val state by component.entitiesList.subscribeAsState()
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-    ) {
-        item { Text("Entities screen") }
-        items(state) { child ->
-            child.Render()
+    Scaffold {
+        val state by component.entitiesList.subscribeAsState()
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.padding(it)
+        ) {
+            item { Text("Entities screen") }
+            items(state) { child ->
+                child.Render()
+            }
         }
     }
 }
