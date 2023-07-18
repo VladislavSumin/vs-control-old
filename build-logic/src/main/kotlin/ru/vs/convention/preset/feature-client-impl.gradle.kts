db711f09fe5b167bcb6fa117d043a91735ce0470
Vladislav Sumin <vladislavsumin@mail.ru>
2023-07-18 22:34:59 +0300

    add factory generator template

--- build-logic/src/main/kotlin/ru/vs/convention/preset/feature-client-impl.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/preset/feature-client-impl.gradle.kts
@@ -9,6 +9,7 @@ import org.gradle.accessors.dm.LibrariesForCoreLibs
 
 plugins {
     id("ru.vs.convention.preset.feature-client-api")
+    id("ru.vs.convention.factory-generator")
     id("org.jetbrains.compose")
 }
 
