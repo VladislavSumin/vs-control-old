38ae47c88e12d6de8283539237b977ead6e28316
Vladislav Sumin <vladislavsumin@mail.ru>
2023-11-01 01:44:38 +0300

    fix DuplicatesStrategy for server

--- server/jvm/build.gradle.kts
+++ server/jvm/build.gradle.kts
@@ -8,27 +8,17 @@ val mainClass: String = "ru.vs.control.MainKt"
 
 kotlin {
     jvm {
-        fatJar(mainClass, jarName = "control")
+        fatJar(
+            mainClass = mainClass,
+            jarName = "control",
+            duplicatesStrategy = DuplicatesStrategy.WARN
+        )
     }
 
     sourceSets {
         named("jvmMain") {
             dependencies {
                 api(projects.server.common)
-
-                // TODO Студия не корректно определяет classpath при запуске через стандартную конфигурацию
-                // как workaround пока нужно тут прописывать явно все зависимости
-//                implementation(coreLibs.vs.core.coroutines)
-//                implementation(coreLibs.vs.core.di)
-//                implementation(coreLibs.vs.core.ktor.server)
-//                implementation(coreLibs.vs.core.logging)
-//                implementation(coreLibs.ktor.server.websocket)
-//
-//                implementation(projects.rsub.connector.ktorWebsocket.server)
-//                implementation(projects.rsub.server)
-//
-//                implementation(coreLibs.kotlin.serialization.core)
-//                implementation(coreLibs.kotlin.serialization.json)
             }
         }
     }
