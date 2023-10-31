43dee6f84f459d338491ce57eb2821bed561c39f
Vladislav Sumin <vladislavsumin@mail.ru>
2023-11-01 00:20:11 +0300

    update ServersViewModel

--- feature/servers/client-impl/src/commonMain/kotlin/ru/vs/control/servers/ui/servers/ServersViewModel.kt
+++ feature/servers/client-impl/src/commonMain/kotlin/ru/vs/control/servers/ui/servers/ServersViewModel.kt
@@ -1,15 +1,23 @@
 package ru.vs.control.servers.ui.servers
 
-import kotlinx.coroutines.flow.SharingStarted
+import androidx.compose.runtime.Stable
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import ru.vs.control.servers.domain.Server
 import ru.vs.control.servers.domain.ServersInteractor
 import ru.vs.core.decompose.viewmodel.ViewModel
 import ru.vs.core.factory_generator.GenerateFactory
 
-@GenerateFactory
-class ServersViewModel(serversInteractor: ServersInteractor) : ViewModel() {
-    val state: StateFlow<List<Server>> = serversInteractor.observeServers()
-        .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+@Stable
+internal interface ServersViewModel {
+    val state: StateFlow<List<Server>>
+}
+
+@GenerateFactory(ServersViewModelFactory::class)
+internal class ServersViewModelImpl(serversInteractor: ServersInteractor) : ViewModel(), ServersViewModel {
+    override val state: StateFlow<List<Server>> = serversInteractor.observeServers()
+        .stateIn(emptyList())
+}
+
+internal interface ServersViewModelFactory {
+    fun create(): ServersViewModelImpl
 }
