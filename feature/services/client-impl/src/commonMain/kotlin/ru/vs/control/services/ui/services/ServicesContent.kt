c765884d2c6567de523b923a9d72eef87f5e687d
Vladislav Sumin <vladislavsumin@mail.ru>
2023-11-05 17:35:02 +0300

    add add button to service screen

--- feature/services/client-impl/src/commonMain/kotlin/ru/vs/control/services/ui/services/ServicesContent.kt
+++ feature/services/client-impl/src/commonMain/kotlin/ru/vs/control/services/ui/services/ServicesContent.kt
@@ -1,10 +1,33 @@
 package ru.vs.control.services.ui.services
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun ServicesContent(component: ServicesComponent) {
-    component
-    Text("Services screen")
+    component // for suppress linter errors
+    Scaffold(
+        floatingActionButton = { AddServiceButton() }
+    ) {
+        Box(Modifier.padding(it)) {
+            Text("Services screen")
+        }
+    }
+}
+
+@Composable
+private fun AddServiceButton() {
+    FloatingActionButton(
+        onClick = {}
+    ) {
+        Icon(Icons.Default.Add, contentDescription = "Add new service")
+    }
 }
