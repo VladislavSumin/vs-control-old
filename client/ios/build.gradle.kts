7f4d09dd8ce9ed10c67bb366434b2f40d9f73c41
Vladislav Sumin <vladislavsumin@mail.ru>
2023-11-05 18:19:06 +0300

    update kotlin version

--- client/ios/build.gradle.kts
+++ client/ios/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.compose.experimental.dsl.IOSDevices
+//import org.jetbrains.compose.experimental.dsl.IOSDevices
 
 plugins {
     id("ru.vs.convention.kmp.ios")
@@ -7,21 +7,22 @@ plugins {
 }
 
 compose.experimental {
-    uikit.application {
-        bundleIdPrefix = "ru.vs"
-        projectName = "Control"
-        deployConfigurations {
-            simulator("IPhone13") {
-
-                // Usage: ./gradlew iosDeployIPhone13Debug
-                device = IOSDevices.IPHONE_13_PRO
-            }
-            simulator("IPadUI") {
-                // Usage: ./gradlew iosDeployIPadUIDebug
-                device = IOSDevices.IPAD_MINI_6th_Gen
-            }
-        }
-    }
+    // TODO выяснить куда это все делось
+//    uikit.application {
+//        bundleIdPrefix = "ru.vs"
+//        projectName = "Control"
+////        deployConfigurations {
+////            simulator("IPhone13") {
+////
+////                // Usage: ./gradlew iosDeployIPhone13Debug
+////                device = IOSDevices.IPHONE_13_PRO
+////            }
+////            simulator("IPadUI") {
+////                // Usage: ./gradlew iosDeployIPadUIDebug
+////                device = IOSDevices.IPAD_MINI_6th_Gen
+////            }
+////        }
+//    }
 }
 
 kotlin {
